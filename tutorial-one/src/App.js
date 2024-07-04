e4724efb7e3eb9643514d378bb6134223c9b8f1d
jinhwa2 <162092896+jinhwa2@users.noreply.github.com>
2024-07-04 11:44:59 +0900

    "statd,context,hook"

--- tutorial-one/src/App.js
+++ tutorial-one/src/App.js
@@ -20,15 +20,20 @@ import R02 from "./R02_FunctionComponent.js";
 import Props1 from "./R03_Prop.js";
 import Props2 from "./R04_Props2.js";
 import Props3 from "./R05_Props3.js";
+import 예제1번 from "./R06_State1.js"; //'' "" 크게 상관 없음
+import 예제2번 from "./R07_State2.js";
+import 예제3번 from "./R08_State3.js";
+import 예제4번 from "./R09_State4.js";
+import 예제5번 from "./R10_State5.js";
 function App() {
   return (
-    /* 
+     /* 
     리액트는 알아서 새로고침을 주기적으로 자주 진행
     주석 :   / * * /   -> 설명이나 추가 내용을 작성할 때 사용
           { / * * /}   -> 주로 코드를 주석 처리할 때 사용
     */
-    <div className="App-header">
-      {/*<R01/>*/}
+          <div className="App-header">
+          {/*<R01/>*/}
       {/*<R02/>*/}
       {/*
       <Props1 num="1" name="홍길동" age="20" score="90" />
@@ -43,15 +48,19 @@ function App() {
       <div className="info">
         이름 : 강감찬 / 나이 : 72 / 성별 : 남자
       </div>
-      */}
-      <Props3 num='5' name="김연정" age='40' gender='여자' phone='010-1111-2222'/>
+          
+          <Props3 num='5' name="김연정" age='40' gender='여자' phone='010-1111-2222'/>
+    */}
+    
+           {/*   <예제1번 /> */}
+      {/*    <예제2번 /> */}
+      {/*   <예제3번 init="50" step="10"/> */}
+      {/*    init = 처음에 시작할 숫자값  step = 몇 씩 숫자가 커지고 작아질 것인가 */}
+      {/*   <예제4번 age="40" years= "1"/> */}
 
-
-
-
-
-    </div>
-  );
-}
-
-export default App;
+            <예제5번/>
+        </div>
+      );
+    }
+    
+    export default App;
